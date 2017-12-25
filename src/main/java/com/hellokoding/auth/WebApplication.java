009990d8e9756f5c00b9e138274cc5af87b9e999
Michael Dalby <mike@Michaels-MacBook-Pro.local>
2017-12-24 19:37:45 -0800

    Removing Spring initializer

--- src/main/java/com/hellokoding/auth/WebApplication.java
+++ src/main/java/com/hellokoding/auth/WebApplication.java
@@ -3,14 +3,14 @@ package com.hellokoding.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(WebApplication.class);
-    }
+//public class WebApplication extends SpringBootServletInitializer {
+public class WebApplication {
+    //@Override
+    //protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    //    return application.sources(WebApplication.class);
+    //}
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(WebApplication.class, args);
